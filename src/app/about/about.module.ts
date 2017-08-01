266070e23bc3e195c9cbcc20e5b023791e77f8fb
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-08-01 15:13:14 -0700

    Add main about compoent, remove borders

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AboutMainComponent } from './about-main/about-main.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [AboutMainComponent]
 })
 export class AboutModule { }
