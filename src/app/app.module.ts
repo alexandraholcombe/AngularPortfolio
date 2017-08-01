b9dfc04c285a38314ba435c18e26b8e0d08d6f2e
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-07-31 17:34:29 -0700

    Tweak styling for development, add navbar component to app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AboutModule } from './about/about.module';
 
-
 //Components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './_components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-  NavbarComponent],
+    AppComponent,
+    NavbarComponent
+  ],
   imports: [
     BrowserModule,
     AboutModule
