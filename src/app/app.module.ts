ecc2663945d0298856750bf04799bb7e13f6419d
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-07-31 17:07:49 -0700

    Generate project, add some npm dependencies, change to use sass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AboutModule } from './about/about.module';
 
 import { AppComponent } from './app.component';
+import { NavbarComponentComponent } from './_components/navbar-component/navbar-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AboutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
